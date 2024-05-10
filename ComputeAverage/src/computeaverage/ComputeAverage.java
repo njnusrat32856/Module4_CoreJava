9bddd5633c41b694785b2571027729d2225d6e4a
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-05-10 21:02:05 +0600

    chapter 2

---
+++ ComputeAverage/src/computeaverage/ComputeAverage.java
@@ -0,0 +1,25 @@
+package computeaverage;
+
+import java.util.Scanner;
+
+public class ComputeAverage {
+
+    public static void main(String[] args) {
+        // Create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enterr three numbers
+        System.out.println("Enter three numbers: ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+
+        // Compute average
+        double average = (number1 + number2 + number3) / 3;
+
+        // Display results
+        System.out.println("The average of " + number1 + " " + number2 + " " + number3 + " is " + average);
+
+    }
+
+}
