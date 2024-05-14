632caaf72737fa9d4118dcfe18a4949b6120e8bb
Nusrat-Naima <n.j.naima222@gmail.com>
2024-05-14 15:15:21 +0600

    chapter-18

---
+++ FactorialWithRecursion/src/factorialwithrecursion/FactorialWithRecursion.java
@@ -0,0 +1,23 @@
+
+package factorialwithrecursion;
+
+import java.util.Scanner;
+
+public class FactorialWithRecursion {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter a non-negative integer: ");
+        
+        int n = input.nextInt();
+        
+        System.out.println("Factorial of " + n+ " is "+ factorial(n));
+    }
+    public static long factorial(int n){
+        if(n == 0)
+            return 1;
+        else{
+            return  n* factorial(n-1);
+        }
+    }
+}
