8d3e628c3802d13b026de8c2f0ec7eb2bbe3ca1f
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-05-16 00:05:48 +0600

    shape calculate
    using super and subclass

---
+++ TestShape/src/shape/Shape.java
@@ -0,0 +1,15 @@
+
+package shape;
+
+public class Shape {
+    // Base class for all shapes
+    public double getArea() {
+        return 0.0; // Default implementation (to be overridden)
+    }
+
+    public double getPerimeter() {
+        return 0.0; // Default implementation (to be overridden)
+    }
+    
+    
+}
---
+++ TestShape/src/testshape/TestShape.java
@@ -0,0 +1,25 @@
+
+package testshape;
+
+import shape.Circle;
+import shape.Rectangle;
+import shape.Triangle;
+
+public class TestShape {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+
+        System.out.println("Triangle Area: " + triangle.getArea());
+        System.out.println("Triangle Perimeter: " + triangle.getPerimeter());
+    }
+    
+}
