9377e627171a6799d82f5c03b753c7e38df493cb
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-05-19 11:37:19 +0600

    chapter-5.12

---
+++ TestBreak/src/testbreak/TestBreak.java
@@ -0,0 +1,27 @@
+
+package testbreak;
+
+public class TestBreak {
+
+    public static void main(String[] args) {
+        
+//        System.out.println("H");
+
+        int sum =0;
+        int number =0;
+        
+        while(number < 20){
+            number++;
+            sum += number;
+            
+            if(sum >= 100){
+                break;
+            }
+        }
+        
+        System.out.println("The number is " + number);
+        
+        System.out.println("The sum is " + sum);
+    }
+    
+}
