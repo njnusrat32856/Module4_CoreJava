ae9a2e95cb84dcab5067ee9d92b08619969bd1b8
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-05-23 20:29:50 +0600

    chapter-9 class and object

---
+++ TestClassAndObject/src/testclassandobject/TestClassAndObject.java
@@ -0,0 +1,19 @@
+
+package testclassandobject;
+
+import math1.CheckCalculator;
+
+public class TestClassAndObject {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hi");
+        
+        CheckCalculator number=new CheckCalculator();
+         double result= number.checkCalculation();
+         
+        System.out.println(result);
+        
+    }
+    
+}
