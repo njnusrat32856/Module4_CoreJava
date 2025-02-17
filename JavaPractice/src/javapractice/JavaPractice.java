163d79fc47a8a82d9a095ec0da8f9e87153fd9a6
njnusrat32856 <nusratjahannaima32856@gmail.com>
2025-02-17 22:16:30 +0600

    Update JavaPractice.java

--- JavaPractice/src/javapractice/JavaPractice.java
+++ JavaPractice/src/javapractice/JavaPractice.java
@@ -18,6 +18,7 @@ public class JavaPractice {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
